1317384a724a87877483c35dba92541fda64abb9
Pathlib <gautroncyber@proton.me>
2026-06-14 19:09:33 +0200

    Enhance data processing and error handling in main.py
    Refactor CSV reading and data analysis with error handling and improved output formatting.

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import pandas as pd
+#lire le fichier csv
+try:
+    df = pd.read_csv("data.csv")
+except FileNotFoundError:
+    print("file not found")
+except Exception as e:
+    print(e)
+    raise Exception 
 
+<<<<<<< HEAD
+=======
 # lire le fichier avec le moteur 'odf'
 try:
     df = pd.read_csv("data.csv")
 except FileNotFoundError:
     print("file not found")
+>>>>>>> 9166aee43e9ffd1f9fbb1e0ddb7f87cb56f1f16b
 
-# afficher les 5 premières lignes
+# afficher le nombre de colonne et ligne du tableaux
 print(df.shape)
+print(df.columns)
 
 df["Revenue"] = df["quantity"] * df["unit_price"] - df["discount"]
 print("Chiffre d'affaires total :", df["Revenue"].sum())
 
+print(f"produit moyen vendu: {df['unit_price'].mean()}")
+print(f"nombre de categorie  ou il y a de l electronique {(df["category"]== "Electronics").sum()}")
+print(f"nombre de categorie ou il y a furniture {(df['category'] == 'Furniture').sum()}")
 
-print(f" produit moyen vendu:  {df["unit_price"].mean()}")
-print((df["category"]== "Electronics").sum())
-print((df["category"] == "Furniture").sum())
 
-
-print(df["customer_age"].mean())
+print(f"la moyen des age est de {df['customer_age'].mean()}")
 f=(df["gender"]== "F").sum()
 m=(df["gender"]== "M").sum()
-print(f,m)
+print(f"il y a {f} femme et {m} homme")
 
-print(df["customer_age"]<25)
-print((df["customer_age"] > 25) & (df["customer_age"] < 50))
-print(df["customer_age"]>=50)
+print(df[df["customer_age"]<25].sum())
+print(df[(df["customer_age"] > 25) & (df["customer_age"] < 50)])
+print((df["customer_age"]==50).sum())
 
-print(((df["returned"]=="Yes").sum()/len(df))*100)
-print(((df["returned"]=="No").sum()/len(df))*100)
+print(f"{((df['returned']=='Yes').sum()/len(df))*100}des utilisateur on renvoyer")
+print(f"{((df['returned']=='No').sum()/len(df))*100}des utilisateur n'on renvoyer")
 
 note_retours = df[df["returned"] == "Yes"]["rating"].mean()
 note_sans_retours = df[df["returned"] == "No"]["rating"].mean()
@@ -42,4 +53,4 @@ print(df[df["category"] == "Furniture"]["rating"].mean())
 print(df["unit_price"].corr(df["rating"]))
 df["unit_price"].dropna()
 donnee=df["unit_price"].max()
-print(f"le print le plus haut est {donnee}")
+print(f"le print le plus �lev� est {donnee}")
